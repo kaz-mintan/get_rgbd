940d59f3da6174b07bde3aa3e66ff0cacb072e9a
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-01-07 20:19:03 +0900

    changed to get r,g,b,d but not create image, yet.

--- capture_rgbd/draw.py
+++ capture_rgbd/draw.py
@@ -1,9 +1,21 @@
 # -*-coding: utf-8 -*-
-
+import os
 import numpy as np
-import cv2
 
-array = np.loadtxt("depth_test.csv", delimiter=",") 
-cv2.imshow("test",array)
-while cv2.waitKey(33) != 27:
-  pass
+path = "/home/kazumi/prog/get_rgbd/capture_rgbd/test_2/rgbd/"
+directory = os.listdir(path)
+
+for i in directory:
+  print(i[2:26])
+  rgb_data = np.zeros((480,640,3),dtype=np.uint8)
+  depth_data = np.zeros((480,640))
+  print(i)
+  if(i[0]=='r'):
+    rgb_data[:,:,2] = np.loadtxt(path+i, delimiter=",") 
+  if(i[0]=='g'):
+    rgb_data[:,:,1] = np.loadtxt(path+i, delimiter=",") 
+  if(i[0]=='b'):
+    rgb_data[:,:,2] = np.loadtxt(path+i, delimiter=",") 
+  if(i[0]=='d'):
+    depth_data = np.loadtxt(path+i, delimiter=",") 
+
