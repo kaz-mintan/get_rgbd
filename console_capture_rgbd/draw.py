2bfd0e14e4776bd93c2bd6783e6e63611c8d7c47
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-01-15 15:07:40 +0900

    updated to read rgb data and generate pic

--- console_capture_rgbd/draw.py
+++ console_capture_rgbd/draw.py
@@ -8,18 +8,20 @@ directory = os.listdir(path)
 
 for i in directory:
   print(i[2:26])
-  rgb_data = np.zeros((480,640,3),dtype=np.uint8)
+  rgb_array= np.zeros((480,640,3),dtype=np.uint8)
+  data = np.zeros((480,640*3),dtype=np.uint8)
   depth_data = np.zeros((480,640))
   print(i)
-  if(i[0]=='r'):
-    rgb_data[:,:,2] = np.loadtxt(path+i, delimiter=",") 
-  if(i[0]=='g'):
-    rgb_data[:,:,1] = np.loadtxt(path+i, delimiter=",") 
-  if(i[0]=='b'):
-    rgb_data[:,:,2] = np.loadtxt(path+i, delimiter=",") 
-  if(i[0]=='d'):
+  if(i[0]=='0'):
+    data = np.loadtxt(path+i, delimiter=",") 
+    rgb_array[:,:,2] = data[:,::3]
+    rgb_array[:,:,1] = data[:,1::3]
+    rgb_array[:,:,0] = data[:,2::3]
+    cv2.imshow("color_test",rgb_array)
+  if(i[0]=='1'):
     depth_data = np.loadtxt(path+i, delimiter=",") 
     cv2.imshow("test",depth_data)
+
   while cv2.waitKey(33) != 27:
     pass
 
